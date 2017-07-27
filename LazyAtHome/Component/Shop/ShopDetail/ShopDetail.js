20ec652a48226174e71c137d8569a9615f450b0d
xurq <lingnet@lingnet.local>
2017-07-27 17:05:21 +0800

    no message

---
+++ LazyAtHome/Component/Shop/ShopDetail/ShopDetail.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lingnet on 2017/7/26.
+ */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+{/*-----引入外部组件--导航---*/}
+import TopNavView from '../../Home/SelectAddress/TopNavView';
+
+ShopDetail = React.createClass({
+    render() {
+        return (
+            <View>
+                <Text>详情页</Text>
+
+            </View>
+        );
+    }
+});
+
+module.exports = ShopDetail;
